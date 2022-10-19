acc102deeda8e08f0edc7cbabf0487f11515890d
Roman Kalganov <kalganovrg2@multibonus.ru>
2022-10-19 21:48:52 +0300

    корова получает имя

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 class Cow:
+
+    def __init__(self, name):
+        self.name = name
+
     def say_something(self):
-        return "МУУУУУУУУ"
+        return f"{self.name} делает МУУУУУУУУ"
 
 
 if __name__ == '__main__':
-    my_cow = Cow()
+    my_cow = Cow("мурка")
     print(my_cow.say_something())
